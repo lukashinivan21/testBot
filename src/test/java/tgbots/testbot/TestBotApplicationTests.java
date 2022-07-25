bcc6549524018d6b925508dfbca2ff7dacda6e14
Ivan <uvanes21@yandex.ru>
2022-07-25 07:57:51 +0300

    Initial commit

---
+++ src/test/java/tgbots/testbot/TestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tgbots.testbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
